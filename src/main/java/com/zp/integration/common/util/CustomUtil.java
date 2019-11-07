921076d979f09a59316b00ea481160f434a74c77
zt_zp <961396523@qq.com>
2019-11-07 19:13:06 +0800

    提供自定义正则校验邮箱和汉字的方法

--- src/main/java/com/zp/integration/common/util/CustomUtil.java
+++ src/main/java/com/zp/integration/common/util/CustomUtil.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomUtil {
 	
@@ -63,6 +65,37 @@ public class CustomUtil {
 		}
 		return shortBuffer.toString();
 	}
+
+	/**
+	 * 验证是否为正确的邮箱号
+	 * @param email
+	 * @return
+	 */
+	public static boolean isEmail(String email) {
+		// 1、\\w+表示@之前至少要输入一个匹配字母或数字或下划线 \\w 单词字符：[a-zA-Z_0-9]
+		// 2、(\\w+\\.)表示域名. 如新浪邮箱域名是sina.com.cn
+		// {1,3}表示可以出现一次或两次或者三次.
+		String reg = "\\w+@(\\w+\\.){1,3}\\w+";
+		Pattern pattern = Pattern.compile(reg);
+		boolean flag = false;
+		if (email != null && !"".equals(email)) {
+			Matcher matcher = pattern.matcher(email);
+			flag = matcher.matches();
+		}
+		return flag;
+	}
+
+	/**
+	 * 正则校验是否为汉字
+	 * @param param
+	 * @return
+	 */
+	public static boolean isChinese(String param) {
+		String regex = "^[\u4E00-\u9FFF()（）]+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(param);
+		return matcher.matches();
+	}
 	
 	
 	public static void main(String[] args) {
@@ -101,7 +134,14 @@ public class CustomUtil {
 		for(String list : typeList) {
 			System.out.println(list);
 		}
-		
+
+		String email = "xxx@allinkgo.com";
+		boolean b = isEmail(email);
+		System.out.println(b);
+
+		String src = "全民出游";
+		boolean res = isChinese(src);
+		System.out.println(res);
 		
 	}
 
